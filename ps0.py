3658ba27417f95b0b280ddac4564fbd70e162407
Vaishnavi Mittal <vishu2001mittal@gmail.com>
2021-09-08 01:12:54 +0530

    feat:ProblemSet#0

---
+++ ps0.py
@@ -0,0 +1,12 @@
+Python 3.9.2 (tags/v3.9.2:1a79785, Feb 19 2021, 13:44:55) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #vaishnavi mittal
+>>> #problem set 0
+>>> #time spent=5 mins
+>>> date=input("Enter your date of birth")
+Enter your date of birth 08/05/2001
+>>> name=input("Enter your last name")
+Enter your last name Mittal
+>>> print(date,name)
+ 08/05/2001  Mittal
+>>> 

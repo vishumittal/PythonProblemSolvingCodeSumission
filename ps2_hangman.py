feb0c1ce3b6dcee30d38ff66f5d5ae17eec6ecd3
Vaishnavi Mittal <vishu2001mittal@gmail.com>
2021-09-22 15:08:26 +0530

    feat:ProblemSet#2

--- ps2_hangman.py
+++ ps2_hangman.py
@@ -1,6 +1,6 @@
 # Problem set 2
 # Name: Vaishnavi Mittal
-# Time spent:5 days
+# Time spent:3 days
 
 
 import random
@@ -8,12 +8,7 @@ import random
 WORDLIST_FILENAME = "D:\PythonProblemSolvingCodeSumission\ps2\words.txt"
 
 def loadWords():
-    """
-    Returns a list of valid words. Words are strings of lowercase letters.
     
-    Depending on the size of the word list, this function may
-    take a while to finish.
-    """
     print("Loading word list from file...")
     # inFile: file
     inFile = open(WORDLIST_FILENAME, 'r')
@@ -25,22 +20,14 @@ def loadWords():
     return wordlist
 
 def chooseWord(wordlist):
-    """
-    wordlist (list): list of words (strings)
-    Returns a word from wordlist at random
-    """
+    
     return random.choice(wordlist)
 
 # -----------------------------------
 wordlist = loadWords()
 
 def isWordGuessed(secretWord, lettersGuessed):
-    '''
-    secretWord: string, the word the user is guessing
-    lettersGuessed: list, what letters have been guessed so far
-    returns: boolean, True if all the letters of secretWord are in lettersGuessed;
-      False otherwise
-    '''
+    
     c=0
     for i in lettersGuessed:
         if i in secretWord:
@@ -52,32 +39,23 @@ def isWordGuessed(secretWord, lettersGuessed):
 
 
 def getGuessedWord(secretWord, lettersGuessed):
-    '''
-    secretWord: string, the word the user is guessing
-    lettersGuessed: list, what letters have been guessed so far
-    returns: string, comprised of letters and underscores that represents
-      what letters in secretWord have been guessed so far.
-    '''
+    
     s=[]
     for i in secretWord:
         if i in lettersGuessed:
             s.append(i)
-    ans=''
+    answer=''
     for i in secretWord:
         if i in s:
-            ans+=i
+            answer+=i
         else:
-            ans+='_ '
-    return ans
+            answer+='_ '
+    return answer
 
 
 
 def getAvailableLetters(lettersGuessed):
-    '''
-    lettersGuessed: list, what letters have been guessed so far
-    returns: string, comprised of letters that represents what letters have not
-      yet been guessed.
-    '''
+    
     import string
     ans=list(string.ascii_lowercase)
     for i in lettersGuessed:
@@ -85,19 +63,8 @@ def getAvailableLetters(lettersGuessed):
     return ''.join(ans)
 
 def hangman(secretWord):
-    '''
-    secretWord: string, the secret word to guess.
-    Starts up an interactive game of Hangman.
-    * At the start of the game, let the user know how many 
-      letters the secretWord contains.
-    * Ask the user to supply one guess (i.e. letter) per round.
-    * The user should receive feedback immediately after each guess 
-      about whether their guess appears in the computers word.
-    * After each round, you should also display to the user the 
-      partially guessed word so far, as well as letters that the 
-      user has not yet guessed.
-    Follows the other limitations detailed in the problem write-up.
-    '''
+
+
     print("Welcome to the game, Hangman!")
     print("I am thinking of a word that is",len(secretWord),"letters long.")
     
